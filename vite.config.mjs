690092c2a0feb8a68e14004bdcedee0782ae6d36
Fiko <firuz0643@gmail.com>
2025-07-29 20:30:53 +0400

    uploaded vite builder, build structure and header links and logo, bg image in headeer ready

---
+++ vite.config.mjs
@@ -0,0 +1,54 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import viteImagemin from 'vite-plugin-imagemin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+	server: {
+		host: 'localhost', // или '0.0.0.0' для внешнего доступа
+		port: 4000,         // нужный порт
+	},
+
+	// build: {
+	// 	rollupOptions: {
+	// 		input: {
+	// 			main: resolve(__dirname, "index.html"),
+	// 			catalog: "catalog.html",
+	// 			about: "about.html",
+	// 			blog: "blog.html",
+	// 		},
+	// 	},
+	// },
+
+	plugins: [
+		viteImagemin({
+			gifsicle: {
+				optimizationLevel: 7,
+				interlaced: false,
+			},
+			optipng: {
+				optimizationLevel: 7,
+			},
+			mozjpeg: {
+				quality: 20,
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: 'removeViewBox',
+					},
+					{
+						name: 'removeEmptyAttrs',
+						active: false,
+					},
+				],
+			},
+		}),
+	],
+});
